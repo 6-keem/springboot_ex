d9b247b4332bfd3ecf775e8d00d477b67687e72d
6-keem <6ukeem@gmail.com>
2024-10-07 16:50:32 +0900

    [feat] add swagger setting

---
+++ src/main/java/com/blog/springboot_ex/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.blog.springboot_ex.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("springdoc-public")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .version("v1.0")
+                .title("FiveGuys API")
+                .description("FiveGuys x Daou Purrio"); //설명
+        return new OpenAPI()
+                .info(info);
+    }
+}
