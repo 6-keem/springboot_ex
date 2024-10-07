3d29eed9d1d2d7b9b33305a07be6cbaa3689f5a5
6-keem <6ukeem@gmail.com>
2024-10-07 17:22:35 +0900

    [feat] add swagger setting

--- src/main/java/com/blog/springboot_ex/config/SwaggerConfig.java
+++ src/main/java/com/blog/springboot_ex/config/SwaggerConfig.java
@@ -1,29 +1,21 @@
 package com.blog.springboot_ex.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition
 @Configuration
 public class SwaggerConfig {
-
     @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("springdoc-public")
-                .pathsToMatch("/**")
-                .build();
-    }
+    public OpenAPI baseOpenAPI(){
 
-    @Bean
-    public OpenAPI openAPI() {
-        Info info = new Info()
-                .version("v1.0")
-                .title("TEST")
-                .description("Swagger-ui 테스트입니다.");
         return new OpenAPI()
-                .info(info);
+                .info(new Info()
+                .title("TEST API")
+                .version("1.0")
+                .description("Swagger Testß"));
     }
 }
